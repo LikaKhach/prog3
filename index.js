9f8cd018901f8ca366a37f1bc466de4dfb077636
LikaKhach <lika.khachatryan@tumo.org>
2022-11-06 17:23:48 +0400

    Test

---
+++ index.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+
+app.use(express.static("./prog3"));
+
+app.get('/', function (req, res) {
+   res.redirect('index.html');
+});
+
+server.listen(3000);
+
+io.on('connection', function (socket) {
+
+   socket.on("send static", function (data) {
+      console.log(data);
+      main(data);
+      
+   });
+});
+
+
+var fs = require("fs");
+
+
+function main( statistic){
+    const object = JSON.stringify(statistic);
+   fs.writeFileSync("index.json", object);
+  
+}
